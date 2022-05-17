2d31ec48ca70f790569f00ca6fbd4942ba4aa4b6
bayuyap <bayusamper.smd@gmail.com>
2022-05-17 10:23:55 +0700

    reset

--- app/Views/aboutus.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('layout/page_layout') ?>
-<?= $this->section('content') ?>
-<!-- Main -->
-<div id="main" class="shell">
-</div>
-<!-- End Main -->
-<?= $this->endSection() ?>
--- app/Views/home.php
+++
@@ -1,4 +0,0 @@
-<?= $this->extend('layout/page_layout') ?>
-<?= $this->section('slider') ?>
-<?= $this->include('layout/slider') ?>
-<?= $this->endSection() ?>
